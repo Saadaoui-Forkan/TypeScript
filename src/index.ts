fdd07d699b8c8d2d0b493ceb81c63b6f507c19b6
unknown <forkan.saadaoui@gmail.com>
2024-08-29 23:20:37 +0100

    Union And Intersection Types

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,39 @@
 /*
   Data Types
-  - Type Assertions
-  --- Sometime Compiler Doesnt Know The Information We Do
-  --- TypeScript Is Not Performing Any Check To Make Sure Type Assertion Is Valid
+  - Union And Intersection Types
+  --- Union Type
+  ------ The | Symbol Is Used To Create The Union => "Or"
+
+  --- Intersection Type
+  ------ Is A Type That Combines Several Types Into One
+  ------ The & Symbol Is Used To Create An Intersection => "And"
+
+  --- If A Union Is An OR, Then An Intersection Is An AND.
 */
 
-// let myImg = document.getElementById("my-img") as HTMLImageElement;
-let myImg = <HTMLImageElement> document.getElementById("my-img");
-console.log(myImg.src);
+// let all: number | string = 100;
 
-let data: any = 1000;
-console.log((data as string).repeat(3));
+type A = {
+  one: string;
+  two: number;
+  three: boolean;
+};
+
+type B = A & {
+  four: number;
+};
+
+type C = {
+  five: boolean;
+};
+
+type mix = A & C;
+
+function getActions(btns: mix) {
+  console.log(`Hello ${btns.one}`);
+  console.log(`Hello ${btns.two}`);
+  console.log(`Hello ${btns.three}`);
+  console.log(`Hello ${btns.five}`);
+}
+
+getActions({ one: "String", two: 100, three: true, five: true });
