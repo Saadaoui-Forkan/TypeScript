9b3cde59012555e423ea388ffa87e5790a9696e2
unknown <forkan.saadaoui@gmail.com>
2024-09-01 18:40:33 +0100

    Type Annotations With Object

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,30 @@
 /*
-  Data Types
-  - Union And Intersection Types
-  --- Union Type
-  ------ The | Symbol Is Used To Create The Union => "Or"
-
-  --- Intersection Type
-  ------ Is A Type That Combines Several Types Into One
-  ------ The & Symbol Is Used To Create An Intersection => "And"
-
-  --- If A Union Is An OR, Then An Intersection Is An AND.
+  Type Annotations With Object
 */
 
-// let all: number | string = 100;
-
-type A = {
-  one: string;
-  two: number;
-  three: boolean;
+let myObject: {
+  readonly username: string;
+  id: number;
+  hire?: boolean;
+  skills: {
+    one: string;
+    two: string;
+  };
+} = {
+  username: "Elzero",
+  id: 100,
+  hire: true,
+  skills: {
+    one: "HTML",
+    two: "CSS",
+  },
 };
 
-type B = A & {
-  four: number;
-};
+// myObject.username = "Osama";
+myObject.id = 101;
+myObject.hire = false;
 
-type C = {
-  five: boolean;
-};
-
-type mix = A & C;
-
-function getActions(btns: mix) {
-  console.log(`Hello ${btns.one}`);
-  console.log(`Hello ${btns.two}`);
-  console.log(`Hello ${btns.three}`);
-  console.log(`Hello ${btns.five}`);
-}
-
-getActions({ one: "String", two: 100, three: true, five: true });
+console.log(myObject.username);
+console.log(myObject.id);
+console.log(myObject.hire);
+console.log(myObject.skills.one);
